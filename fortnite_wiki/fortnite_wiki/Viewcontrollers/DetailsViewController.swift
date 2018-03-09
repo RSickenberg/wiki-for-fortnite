164f28767f1aff0cf6b014c10cf7d63a11635f82
Romain Sickenberg <romain.sickenberg@liip.ch>
2018-03-09 17:42:18 +0100

    First Table on detail page + bigger image, data is displayed

--- fortnite_wiki/fortnite_wiki/Viewcontrollers/DetailsViewController.swift
+++ fortnite_wiki/fortnite_wiki/Viewcontrollers/DetailsViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Romain Sickenberg on 05.03.18.
 //  Copyright © 2018 Romain Sickenberg. All rights reserved.
+//  TODO: Find a way to reduce all the calls inside the model
 //
 
 import UIKit
@@ -13,6 +14,7 @@ class DetailsViewController: UIViewController {
     
     let colors = BackgroundColors()
 
+    @IBOutlet weak var titleNavigation: UINavigationItem!
     @IBOutlet weak var detailsViewTitle: UINavigationItem!
     @IBOutlet weak var levelOfWeaponSwitch: UISegmentedControl!
     @IBAction func levelOfWeaponSwitch(_ sender: UISegmentedControl) {
@@ -20,11 +22,35 @@ class DetailsViewController: UIViewController {
         let feedback = UISelectionFeedbackGenerator()
         feedback.selectionChanged()
         
-        print(levelOfWeaponSwitch.titleForSegment(at: levelOfWeaponSwitch.selectedSegmentIndex)!)
-        weaponDetails.detailLevel = levelOfWeaponSwitch.selectedSegmentIndex
+        switch levelOfWeaponSwitch.titleForSegment(at: levelOfWeaponSwitch.selectedSegmentIndex)! {
+        case "Common":
+            populateLabelsByValueAndLevels(0)
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "969696")!, UIColor.clear])
+            break
+        case "Atypical":
+            populateLabelsByValueAndLevels(1)
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "4FCA00")!, UIColor.clear])
+        case "Rare":
+            populateLabelsByValueAndLevels(2)
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "00BFFF")!, UIColor.clear])
+        case "Epic":
+            populateLabelsByValueAndLevels(3)
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "B83DF2")!, UIColor.clear])
+        case "Legendary":
+            populateLabelsByValueAndLevels(4)
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "E6BB0E")!, UIColor.clear])
+        default:
+            break
+        }
     }
     @IBOutlet weak var weaponImage: UIImageView!
-    @IBOutlet weak var UIBackgroundView: UIView!
+    @IBOutlet weak var uiBackgroundView: UIView!
+    @IBOutlet weak var damageLabel: UILabel!
+    @IBOutlet weak var headLabel: UILabel!
+    @IBOutlet weak var fireRateLabel: UILabel!
+    @IBOutlet weak var magazineSizeLabel: UILabel!
+    @IBOutlet weak var impactLabel: UILabel!
+    @IBOutlet weak var reloadLabel: UILabel!
     
     var index: Int = 0
     var weaponInfo = Weapons()
@@ -35,12 +61,13 @@ class DetailsViewController: UIViewController {
         super.viewDidLoad()
 
         backgroundGradient()
-        
         detailsViewTitle.title = weaponInfo.weaponName
         weaponImage.image = UIImage(named: weaponInfo.weaponImg)
+        weaponImage.layer.zPosition = 1
+        levelOfWeaponSwitch.layer.zPosition = 0.9
         
-        getGradientValueforBackgroundImage()
         prepareSegment()
+        getGradientValueforBackgroundImage()
     }
     
     func backgroundGradient() {
@@ -66,23 +93,42 @@ class DetailsViewController: UIViewController {
         
         switch weaponDetails.detailLevel {
         case 0 :
-            UIBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: UIBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "969696")!, UIColor.clear])
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "969696")!, UIColor.clear])
+            populateLabelsByValueAndLevels(0)
             break
         case 1 :
-            UIBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: UIBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "4FCA00")!, UIColor.clear])
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "4FCA00")!, UIColor.clear])
+            populateLabelsByValueAndLevels(1)
             break
         case 2 :
-            UIBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: UIBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "00BFFF")!, UIColor.clear])
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "00BFFF")!, UIColor.clear])
+            populateLabelsByValueAndLevels(2)
             break
         case 3 :
-            UIBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: UIBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "B83DF2")!, UIColor.clear])
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "B83DF2")!, UIColor.clear])
+            populateLabelsByValueAndLevels(3)
             break
         case 4 :
-            UIBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: UIBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "E6BB0E")!, UIColor.clear])
+            uiBackgroundView.backgroundColor = GradientColor(UIGradientStyle.topToBottom, frame: uiBackgroundView.frame, colors: [UIColor.clear ,UIColor.init(hexString: "E6BB0E")!, UIColor.clear])
+            populateLabelsByValueAndLevels(4)
             break
         default:
             break
         }
+    }
+    
+    func populateLabelsByValueAndLevels(_ level: Int) {
+        //let listOfLevels = weaponModel.getLevelsByWeaponId(weaponInfo.weaponId)
+        
+        let details = weaponModel.getDetailsByWeaponIdAndLevel(weaponId: weaponInfo.weaponId, weaponLevel: level)
+        
+        damageLabel.text = String(details.damage)
+        headLabel.text = String(details.damageHead)
+        fireRateLabel.text = String(details.fireRate)
+        magazineSizeLabel.text = String(details.magazineSize)
+        impactLabel.text = String(details.impact)
+        reloadLabel.text = String(details.reloadTime)
+        
     }
 
     /*
