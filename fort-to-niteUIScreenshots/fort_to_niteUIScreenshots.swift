c81a3a0da950331f7fed8c786412528d25fa1cc5
Romain Sickenberg <romain.sickenberg@liip.ch>
2018-08-03 18:47:12 +0200

    UIScreenshots tests is better

--- fort-to-niteUIScreenshots/fort_to_niteUIScreenshots.swift
+++ fort-to-niteUIScreenshots/fort_to_niteUIScreenshots.swift
@@ -17,8 +17,9 @@ class fort_to_niteUIScreenshots: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        if let bundleID = Bundle.main.bundleIdentifier {
+            UserDefaults.standard.removePersistentDomain(forName: bundleID)
+        }
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         let app = XCUIApplication()
@@ -29,50 +30,42 @@ class fort_to_niteUIScreenshots: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        
+        if let bundleID = Bundle.main.bundleIdentifier {
+            UserDefaults.standard.removePersistentDomain(forName: bundleID)
+        }
     }
     
     func testExample() {
         let app = XCUIApplication()
-        let collectionViewsQuery = app.collectionViews
-        snapshot("01WeaponCollectionView")
-        collectionViewsQuery.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
-        
-        let assaultRifleBurstNavigationBar = app.navigationBars["Assault Rifle (burst)"]
-        assaultRifleBurstNavigationBar.buttons["Item"].tap()
+        snapshot("01WeaponsCollections")
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
+        app.navigationBars["Assault Rifle (burst)"].buttons["Item"].tap()
         sleep(3)
-        assaultRifleBurstNavigationBar.buttons["WEAPONS"].tap()
-        collectionViewsQuery.children(matching: .cell).element(boundBy: 2).children(matching: .other).element.tap()
         
-        let assaultRifleScarNavigationBar = app.navigationBars["Assault Rifle (SCAR)"]
-        snapshot("02AssaultRiffle")
-        assaultRifleScarNavigationBar.buttons["Item"].tap()
+        app.tabBars.buttons["Weapons"].tap()
+        app.collectionViews.children(matching: .cell).element(boundBy: 2).children(matching: .other).element.tap()
+        snapshot("02Weapon1")
+        
+        let itemButton = app.navigationBars["Assault Rifle (SCAR)"].buttons["Item"]
+        itemButton.tap()
         sleep(3)
-        assaultRifleScarNavigationBar.buttons["WEAPONS"].tap()
-        collectionViewsQuery.children(matching: .cell).element(boundBy: 3).children(matching: .other).element.tap()
         
-        let assaultRifleScopedNavigationBar = app.navigationBars["Assault Rifle (Scoped)"]
-        assaultRifleScopedNavigationBar.buttons["Item"].tap()
-        sleep(3)
-        assaultRifleScopedNavigationBar.buttons["WEAPONS"].tap()
-        
-        let tabBarsQuery = app.tabBars
-        let itemsButton = tabBarsQuery.buttons["Items"]
-        itemsButton.tap()
-        snapshot("03ItemCollectionView")
-        collectionViewsQuery.children(matching: .cell).element(boundBy: 6).children(matching: .other).element.tap()
+        let tabBarsQuery = XCUIApplication().tabBars
+        tabBarsQuery.buttons["Weapons"].tap()
+        tabBarsQuery.buttons["Items"].tap()
+        snapshot("03ItemsCollections")
+        app.collectionViews.children(matching: .cell).element(boundBy: 6).children(matching: .other).element.tap()
+        snapshot("04Item1")
         app.navigationBars["Slurp juice"].buttons["Item"].tap()
-        snapshot("04SlurpJuice")
-        itemsButton.tap()
-        
-        collectionViewsQuery.children(matching: .cell).element(boundBy: 7).children(matching: .other).element.tap()
-        
-        let chugJuiceNavigationBar = app.navigationBars["Chug juice"]
-        chugJuiceNavigationBar.buttons["Item"].tap()
         sleep(3)
-        chugJuiceNavigationBar.buttons["ITEMS"].tap()
-        tabBarsQuery.buttons["More"].tap()
-        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Your Favorites."]/*[[".cells.staticTexts[\"Your Favorites.\"]",".staticTexts[\"Your Favorites.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        snapshot("05FavoritesTable")
+        
+        app.tabBars.buttons["Items"].tap()
+        app.collectionViews.children(matching: .cell).element(boundBy: 4).children(matching: .other).element.tap()
+        app.navigationBars["Medium shield"].buttons["Item"].tap()
+        sleep(3)
+        app.tabBars.buttons["More"].tap()
+        snapshot("05Favorites")
     }
     
 }
