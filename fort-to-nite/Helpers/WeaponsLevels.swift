d7a31c9796a0de9aea3322d3e61e6b1ad953b167
Romain Sickenberg <romain.sickenberg@liip.ch>
2018-12-11 14:51:07 +0100

    Fixed missing colors combinations

--- fort-to-nite/Helpers/WeaponsLevels.swift
+++ fort-to-nite/Helpers/WeaponsLevels.swift
@@ -62,6 +62,9 @@ class FormatLevels {
         case [1, 2, 3]:
             cell.backgroundColor = GradientColor(UIGradientStyle.diagonal, frame: cell.frame, colors: [HexColor("4FCA00")!, HexColor("00BFFF")!, HexColor("B83DF2")!])
             break
+        case [1, 2, 3, 4]:
+            cell.backgroundColor = GradientColor(UIGradientStyle.diagonal, frame: cell.frame, colors: [HexColor("4FCA00")!, HexColor("4FCA00")!, HexColor("00BFFF")!, HexColor("B83DF2")!, HexColor("E6BB0E")!])
+            break
         case [2, 3]:
             cell.backgroundColor = GradientColor(UIGradientStyle.diagonal, frame: cell.frame, colors: [HexColor("00BFFF")!, HexColor("B83DF2")!])
             break
@@ -122,7 +125,7 @@ class FormatLevels {
              cell.backgroundColor = GradientColor(UIGradientStyle.diagonal, frame: cell.frame, colors: [HexColor("4FCA00")!, HexColor("4FCA00")!, HexColor("00BFFF")!, HexColor("B83DF2")!])
              break
         case [1, 2, 3, 4]:
-            cell.backgroundColor = GradientColor(UIGradientStyle.diagonal, frame: cell.frame, colors: [HexColor("4FCA00")!, HexColor("4FCA00")!, HexColor("00BFFF")!, HexColor("B83DF2")!, HexColor("E6BB0E")!])
+            cell.backgroundColor = GradientColor(UIGradientStyle.diagonal, frame: cell.frame, colors: [HexColor("4FCA00")!, HexColor("4FCA00")!, HexColor("4FCA00")!, HexColor("00BFFF")!, HexColor("B83DF2")!, HexColor("E6BB0E")!])
             break
         case [2, 3]:
             cell.backgroundColor = GradientColor(UIGradientStyle.diagonal, frame: cell.frame, colors: [HexColor("00BFFF")!, HexColor("B83DF2")!])
