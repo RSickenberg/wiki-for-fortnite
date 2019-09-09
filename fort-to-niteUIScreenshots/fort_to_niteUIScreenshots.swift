070e482fe61a39a05991514b1374ad76505e3aba
Sickenberg <romain.sickenberg@liip.ch>
2019-09-09 15:43:19 +0200

    Updated snapshot script

--- fort-to-niteUIScreenshots/fort_to_niteUIScreenshots.swift
+++ fort-to-niteUIScreenshots/fort_to_niteUIScreenshots.swift
@@ -39,26 +39,27 @@ class fort_to_niteUIScreenshots: XCTestCase {
     func testExample() {
         let app = XCUIApplication()
         app.buttons["Discover your app."].tap()
-        sleep(5)
+        app.staticTexts["Ok"].tap()
+        sleep(2)
         snapshot("01Main")
+        
         let collectionViewsQuery = app.collectionViews
         collectionViewsQuery.children(matching: .cell).element(boundBy: 2).children(matching: .other).element.children(matching: .image).element.tap()
         snapshot("02WpFv")
+        app.statusBars.children(matching: .other).element.children(matching: .other).element(boundBy: 0).tap()
         app.navigationBars["Assault Rifle (SCAR)"].buttons["Item"].tap()
         
         let tabBarsQuery = app.tabBars
         tabBarsQuery.buttons["Items"].tap()
         sleep(3)
         snapshot("03Itm")
-        collectionViewsQuery.children(matching: .cell).element(boundBy: 6).children(matching: .other).element.children(matching: .image).element.tap()
-        snapshot("03shld")
-        app.navigationBars["Medium Shield"].buttons["Item"].tap()
-        sleep(2)
+        app.navigationBars["Chug Splash"].buttons["Item"].tap()
+        snapshot("04Splash")
         tabBarsQuery.buttons["Favorites"].tap()
-        snapshot("04Fav")
+        snapshot("05Fav")
         tabBarsQuery.buttons["Market"].tap()
         sleep(6)
-        snapshot("05Market")
+        snapshot("06Market")
     }
     
 }
